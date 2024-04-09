0bc095a0100ce42af9e85d2151e54a18bff8a497
SonjaCodes <sonja@kolmer.cc>
2024-04-09 15:50:42 +0200

    added index.js file, linked to the html file and added event listener to search input

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  console.log(searchInput.valut);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
